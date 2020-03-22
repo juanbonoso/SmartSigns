dec7d61ac1aafcdd6abc37029bcbe9eee89e814d
Juan Bonoso <juanbonoso@gmail.com>
2020-03-22 10:58:09 -0500

    Character3D -- Maquetacion de pagina donde integrara el personaje 3D -- Diseño del HTML Y CSS

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import Vue from "vue";
 // import App from "./App.vue";
-import Three from "./pages/Three.vue";
+import Character3D from "./pages/Character3D.vue";
 import store from "./store";
 
 Vue.config.productionTip = false;
 
 new Vue({
   store,
-  render: h => h(Three)
+  render: h => h(Character3D)
 }).$mount("#app");
---
+++ src/pages/Character3D.vue
@@ -0,0 +1,61 @@
+<template>
+  <html>
+    <head>
+      <title>Discoverthreejs.com - Ch 1.7: Load Models</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charset="UTF-8" />
+      <link rel="icon" href="https://discoverthreejs.com/favicon.ico" type="image/x-icon" />
+    </head>
+
+    <body>
+      <div id="app">
+        <h1 class="title"><a href="https://discoverthreejs.com/">Discoverthreejs.com</a> - Ch 1.7 Load Models</h1>
+        <div id="scene-container">
+          <!-- This div will hold our scene-->
+        </div>
+      </div>
+    </body>
+  </html>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { ThreeJSTest } from '@/utils/ThreeTest';
+
+@Component
+export default class Character3D extends Vue {
+  mounted() {
+    const threeJSTest = new ThreeJSTest();
+    threeJSTest.render();
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0px;
+  overflow: hidden;
+  color: white;
+  text-align: center;
+}
+
+h1 {
+  position: absolute;
+  width: 100%;
+  z-index: 1;
+  font-size: 1.5rem;
+}
+
+a {
+  color: white;
+}
+a:hover {
+  color: purple;
+}
+
+#scene-container {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+</style>
