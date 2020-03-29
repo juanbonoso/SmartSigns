a8c1df80ee7931de1e26572103b918001d0b928b
Juan Bonoso <juanbonoso@gmail.com>
2020-03-29 18:18:44 -0500

    Speech -- Maquetacion inicial para conectarse con el Api de Google Package.json -- Actualizacion de dependencias de desarrollo

--- src/pages/Speech.vue
+++ src/pages/Speech.vue
@@ -1,21 +1,74 @@
 <template>
   <div id="app">
-    <h1>Hola</h1>
+    <div id="content">
+      <table width="100%">
+        <tr>
+          <td></td>
+          <td><h1 style="font-weight:500;">Microsoft Cognitive Services Speech SDK JavaScript Quickstart</h1></td>
+        </tr>
+        <tr>
+          <td align="right">
+            <a href="https://docs.microsoft.com/azure/cognitive-services/speech-service/get-started" target="_blank"
+              >Subscription</a
+            >:
+          </td>
+          <td><input id="subscriptionKey" type="text" size="40" :value="subscription" /></td>
+        </tr>
+        <tr>
+          <td align="right">Region</td>
+          <td><input id="serviceRegion" type="text" size="40" :value="region" /></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><button id="startRecognizeOnceAsyncButton" @click="startSpeech">Start recognition</button></td>
+        </tr>
+        <tr>
+          <td align="right" valign="top">Results</td>
+          <td><textarea id="phraseDiv" style="display: inline-block;width:500px;height:200px"></textarea></td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import * as SpeechSDK from 'microsoft-cognitiveservices-speech-sdk';
 
 @Component
-export default class Speech extends Vue {}
+export default class Speech extends Vue {
+  private subscription = '12e56a3353324558b39d9131cc5e2620';
+  private region = 'westus';
+  private phraseDiv!: HTMLTextAreaElement;
+  private startRecognizeOnceAsyncButton!: HTMLButtonElement;
+  private recognizer!: SpeechSDK.SpeechRecognizer;
+  startSpeech() {
+    this.startRecognizeOnceAsyncButton = document.getElementById('startRecognizeOnceAsyncButton') as HTMLButtonElement;
+    const speechConfig = SpeechSDK.SpeechConfig.fromSubscription(this.subscription, this.region);
+    speechConfig.speechRecognitionLanguage = 'es-ES';
+    const audioConfig = SpeechSDK.AudioConfig.fromDefaultMicrophoneInput();
+    this.recognizer = new SpeechSDK.SpeechRecognizer(speechConfig, audioConfig);
+    const self = this;
+    this.recognizer.recognizeOnceAsync(
+      result => {
+        self.startRecognizeOnceAsyncButton.disabled = false;
+        self.phraseDiv.innerHTML += result.text;
+        window.console.log(result);
+
+        self.recognizer.close();
+        // self.recognizer = undefined;
+      },
+      err => {
+        self.startRecognizeOnceAsyncButton.disabled = false;
+        self.phraseDiv.innerHTML += err;
+        window.console.log(err);
+
+        self.recognizer.close();
+        // self.recognizer = undefined;
+      },
+    );
+  }
+}
 </script>
 
-<style>
-h1 {
-  position: absolute;
-  width: 100%;
-  z-index: 1;
-  font-size: 1.5rem;
-}
-</style>
+<style></style>
